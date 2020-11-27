ed108ccee589f73f5c7896b309cc174bbbc44c68
Arthur Vila Nova <arthurjvnb@yahoo.com.br>
2020-11-27 12:49:01 -0300

    Aula 38: POO - Classes e MVC
    Nesse commit temos os avanços das aulas 37 (Trabalhando com Funções) e 38 (POO - Classes e MVC), sendo criado também o primeiro model, User.

--- index.js
+++ index.js
@@ -1,7 +1,22 @@
 let user = {};
 let fields = document.querySelectorAll('#form-user-create [name]');
 
+function addLine(userData) {
+    console.log(userData);
+    document.querySelector('#table-users').innerHTML = 
+    `<tr>
+        <td><img src="dist/img/user1-128x128.jpg" alt="User Image" class="img-circle img-sm"></td>
+        <td>${userData.name}</td>
+        <td>${userData.email}</td>
+        <td>${userData.admin}</td>
+        <td>${userData.birth}</td>
+        <td>
+        <button type="button" class="btn btn-primary btn-xs btn-flat">Editar</button>
+        <button type="button" class="btn btn-danger btn-xs btn-flat">Excluir</button>
+        </td>
+    </tr>`; // 'tr' significa 'table row'
 
+}
 
 document.querySelector('#form-user-create').addEventListener('submit', event => {
     event.preventDefault();
@@ -20,7 +35,9 @@ document.querySelector('#form-user-create').addEventListener('submit', event => 
         
     });
 
-    console.log(user);
+    let userData = new User(user.name, user.gender, user.birth, user.country, user.email, user.password, user.photo, user.admin);
+
+    addLine(userData);
 })
 
 //#region Formas diferentes de selecionar um elemento no documento HTML
